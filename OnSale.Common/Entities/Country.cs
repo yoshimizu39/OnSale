d6c084b009a8af82b6b806cfc222ef2971f5c57b
Yoshi Muñoz Shimizu <yoshimizu39@outlook.es>
2020-08-13 22:01:18 -0500

    Entidad Country

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no debe contener mas de {1} caracteres")]
+        [Required]
+        public string Name { get; set; }
+    }
+
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //creamos un ìndice ùnico para que no se repita
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+
+}
